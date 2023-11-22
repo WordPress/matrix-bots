becdb36eff59552aba41cdd368981e79df71521a
Paulo Pinto <paulo.pinto@automattic.com>
2023-11-22 19:05:56 +0000

    Rename plugin to example

--- plugins/example/example.py
+++ plugins/example/example.py
@@ -3,7 +3,7 @@ from maubot import Plugin, MessageEvent
 from maubot.handlers import event
 
 
-class HelloWorldBot(Plugin):
+class ExampleBot(Plugin):
     @event.on(EventType.ROOM_MESSAGE)
     async def handler(self, message: MessageEvent) -> None:
         if not message.content.body.startswith("Hello, World!"):
