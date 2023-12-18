1fe38b158d6506559b419a37dedd456100a01360
Ashfame <mail@ashfame.com>
2023-12-18 23:38:50 +0400

    improve watchdog (v1.0.1):
    - cache all details of room and not just names
    - retry network failures with exponential delays
    - posts notices in room when shutting down and when starting monitoring
    - introduce config var to control monitoring frequency
    - change formatting of message that gets posted
    - alerts for changes in name and topic of rooms

--- plugins/watchdog/watchdog.py
+++ plugins/watchdog/watchdog.py
@@ -1,13 +1,15 @@
 from maubot import Plugin
 from mautrix.types import RoomID, DirectoryPaginationToken
+from mautrix.types.misc import PublicRoomInfo
 from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
-from typing import Type, List
+from typing import Type, List, Dict
 import asyncio
 
 
 class Config(BaseProxyConfig):
     def do_update(self, helper: ConfigUpdateHelper) -> None:
         helper.copy("room")
+        helper.copy("monitoring_interval")
 
 
 class WatchDog(Plugin):
@@ -27,60 +29,121 @@ class WatchDog(Plugin):
         await super().start()
         self.config.load_and_update()
 
+        await self.post_notice("🔔 watchdog now running")
+
         # Don't forget to cancel the task, when stopping
         self.monitor_rooms_task = self.loop.create_task(self.monitor_rooms())
 
     async def pre_stop(self) -> None:
         if self.monitor_rooms_task is not None and not self.monitor_rooms_task.done():
             self.monitor_rooms_task.cancel()
+        await self.post_notice("🔔 watchdog shutting down")
 
     def get_room_from_config(self) -> RoomID:
         return RoomID(self.config["room"])
 
+    def get_monitoring_interval_from_config(self) -> int:
+        return self.config["monitoring_interval"]
+
     async def monitor_rooms(self) -> None:
-        # Get the initial list of rooms on the homeserver
-        initial_rooms = await self.get_all_rooms_ids()
+        # can't do anything without having an initial list,
+        # so high max_retries effectively not really needing to catch exception
+        known_rooms = await self.query_room_dir(max_retries=999)
+        known_rooms_ids = known_rooms.keys()
 
         while True:
-            current_rooms = await self.get_all_rooms_ids()
+            try:
+                current_rooms = await self.query_room_dir(max_retries=5)
+                current_rooms_ids = current_rooms.keys()
 
-            rooms_removed = list(set(initial_rooms) - set(current_rooms))
-            rooms_added = list(set(current_rooms) - set(initial_rooms))
+                # figure out rooms added/removed
+                rooms_removed = list(set(known_rooms_ids) - set(current_rooms_ids))
+                rooms_added = list(set(current_rooms_ids) - set(known_rooms_ids))
 
-            await self.handle_room_changes(rooms_removed, "Rooms removed:")
-            await self.handle_room_changes(rooms_added, "Rooms added:")
+                await self.handle_room_changes(rooms_removed, "➖ Rooms removed:")
+                await self.handle_room_changes(rooms_added, "➕ Rooms added:")
 
-            initial_rooms = current_rooms
-            await asyncio.sleep(300)  # sleep for 5mins
+                # figure out changes in rooms' name or topic
+                await self.handle_room_meta_changes(known_rooms, current_rooms)
+
+                # update new state as known state
+                known_rooms = current_rooms
+                known_rooms_ids = current_rooms_ids
+
+            except Exception as e:
+                self.log.exception("failed to fetch room dir while monitoring")
+
+            await asyncio.sleep(self.get_monitoring_interval_from_config())
 
     async def handle_room_changes(self, room_list: List[RoomID], message_prefix: str) -> None:
         if room_list:
-            text_message = f"{message_prefix}\n"
-            html_message = f"{message_prefix}<br><ul>"
+            text_message = f"**{message_prefix}**\n"
+            html_message = f"<strong>{message_prefix}</strong><br><ul>"
             for room_id in room_list:
-                text_message += f"{self._cache_room_details[room_id]} `{room_id}`\n"
-                html_message += f"<li>{self._cache_room_details[room_id]} <code>{room_id}</code></li>"
+                text_message += f"{self._cache_room_details[room_id].name} `{room_id}`\n"
+                html_message += f"<li>{self._cache_room_details[room_id].name} <code>{room_id}</code></li>"
             html_message += f"</ul>"
-            await self.client.send_text(room_id=self.get_room_from_config(), text=text_message, html=html_message)
+            await self.post_message(text_message, html_message)
 
-    async def get_all_rooms_ids(self) -> List[RoomID]:
-        all_room_ids = []
+    async def handle_room_meta_changes(self, known_rooms: Dict[RoomID, PublicRoomInfo],
+                                       current_rooms: Dict[RoomID, PublicRoomInfo]):
+        for room_id in current_rooms:
+            if room_id in known_rooms and current_rooms[room_id].name != known_rooms[room_id].name:
+                text_message = f"**Room name update:**\n`{known_rooms[room_id].name}` -> `{current_rooms[room_id].name}` `{room_id}`"
+                html_message = f"<strong>Room name update:</strong><br><code>{known_rooms[room_id].name}</code> -> <code>{current_rooms[room_id].name}</code><br><code>{room_id}</code>"
+                await self.post_message(text_message, html_message)
+
+            if room_id in known_rooms and current_rooms[room_id].topic != known_rooms[room_id].topic:
+                text_message = f"**Room topic update:**\n`{current_rooms[room_id].name}`\n`{known_rooms[room_id].topic}` -> `{current_rooms[room_id].topic}` `{room_id}`"
+                html_message = f"<strong>Room topic update:</strong><br><code>{current_rooms[room_id].name}</code><br>Old topic: <code>{known_rooms[room_id].topic}</code><br>New topic:<code>{current_rooms[room_id].topic}</code><br><code>{room_id}</code>"
+                await self.post_message(text_message, html_message)
+
+    async def query_room_dir(self, max_retries) -> Dict[RoomID, PublicRoomInfo]:
+        all_rooms = {}
+
+        retries = 0
+        base_delay = 10
+        max_delay = 1800  # 30mins
+
         pagination_token = DirectoryPaginationToken("")
 
+        # infinite loop since we need to loop over paginated results as well as upon failures (exceptions)
         while True:
-            directory = await self.client.get_room_directory(
-                limit=1000,
-                include_all_networks=False,
-                since=pagination_token
-            )
+            delay = min(base_delay * 2 ** retries, max_delay)
+
+            try:
+                directory = await self.client.get_room_directory(
+                    limit=1000,
+                    include_all_networks=False,
+                    since=pagination_token
+                )
+            except Exception as e:
+                if retries < max_retries:
+                    self.log.exception("failed to query room directory but will retry")
+                    await asyncio.sleep(delay)
+
+                    retries += 1
+                    continue
+                else:
+                    self.log.exception("failed to query room directory & exhausted max_retries")
+                    raise
+
+            retries = 0
+
             if len(directory.chunk) > 0:
                 for room in directory.chunk:
-                    all_room_ids.append(room.room_id)
-                    self._cache_room_details[room.room_id] = room.name
+                    all_rooms[room.room_id] = room
+                    self._cache_room_details[room.room_id] = room
 
             if directory.next_batch is None:
                 break
 
             pagination_token = directory.next_batch
 
-        return all_room_ids
+        return all_rooms
+
+    async def post_message(self, text, html=None):
+        await self.client.send_text(room_id=self.get_room_from_config(), text=text, html=html)
+
+    async def post_notice(self, text, html=None):
+        await self.client.send_notice(room_id=self.get_room_from_config(), text=text, html=html)
